86ba3e798b70c31ee4787190ea3690ede84cbcc3
clowkun <konpah23@yahoo.com>
2014-08-25 21:25:24 -0700

    Set a template to render all Notes in the database.

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'myproject.notes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,6 +1,8 @@
 # Default application views
 
 from django.shortcuts import render_to_response
+from myproject.notes.models import Note
 
 def hello( request ):
-    return render_to_response('index.html')
+    notes = Note.objects.all()
+    return render_to_response('index.html', {'notes': notes})
